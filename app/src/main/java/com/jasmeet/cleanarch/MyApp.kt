0cb236fc09f21c2ce1538ea3a2463f7c7d261033
jasmeet singh <sjasmeet438@gmail.com>
2024-03-18 16:20:58 +0530

    basic demo done

--- app/src/main/java/com/jasmeet/cleanarch/MyApp.kt
+++ app/src/main/java/com/jasmeet/cleanarch/MyApp.kt
@@ -3,5 +3,8 @@ package com.jasmeet.cleanarch
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+* Created by Jasmeet Singh
+*/
 @HiltAndroidApp
 class MyApp : Application()
